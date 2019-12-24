a991609c4faff048bbfbdef8004aeb5354edc898
cxx <xx.ch@outlook.com>
2019-12-24 21:25:58 +0800

    [mod] rename

--- netbird/src/main/java/cc/colorcat/netbird/Version.java
+++ netbird/src/main/java/cc/colorcat/netbird/Version.java
@@ -24,7 +24,7 @@ package cc.colorcat.netbird;
 final class Version {
 
     static String userAgent() {
-        return "NetBird/4.3.4";
+        return "NetBird/5.0.0";
     }
 
     private Version() {
